f3e4b979097d440e8992b24a2c3c3ee862f32ec1
charlessutikno <charles.sutikno@servian.com>
2021-07-26 13:57:37 +1000

    update name of bucket

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -10,7 +10,7 @@ provider "google" {
   region = "australia-southeast1-a"
 }
 
-resource "google_storage_bucket" "sutikno-gcp-code" {
+resource "google_storage_bucket" "sutikno-gcp-code-batch" {
   name = "batch-pipeline"
   storage_class = "REGIONAL"
   location  = "australia-southeast1"
