6be47f9081b17590fef9837a9f1580d7868c188f
charles <charlessutikno@gmail.com>
2021-08-03 21:54:22 +1000

    fix bugs

--- cloud-function/index.js
+++ cloud-function/index.js
@@ -3,17 +3,17 @@ const google = require('googleapis');
 //var {google} = require('googleapis');
 exports.goWithTheDataFlow = function(data, context, callback) {
   const file = data;  
-  const etype = data.context.eventType;
+  //const etype = data.context.eventType;
   const ttype = context.eventType;
      
   console.log("File is: ", file);
-  console.log("etype is: ", etype);
+  //console.log("etype is: ", etype);
   console.log("ttype is: ", ttype);
-  console.log("file.data.name is: ", file.data.name);
-  console.log("data.name is: ", data.name);
+  //console.log("file.data.name is: ", file.data.name);
+  //console.log("data.name is: ", data.name);
 
   //if (etype === 'google.storage.object.finalize' && file.data.name.indexOf('upload/') !== -1) {
-  if (etype === 'google.storage.object.finalize' && file.data.name.indexOf('upload/') !== -1) {    
+  if (ttype === 'google.storage.object.finalize' && data.name.indexOf('upload/') !== -1) {    
     console.log("inside if");
     google.auth.getApplicationDefault(function (err, authClient, projectId) {
     console.log("right here");
