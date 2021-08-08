b5dafd5c9bea6cf3b4a5dc1fb610612b1adc0e8f
charles <charlessutikno@gmail.com>
2021-08-08 20:46:13 +1000

    change to new version

--- cloud-function/index.js
+++ cloud-function/index.js
@@ -1,53 +1,40 @@
 //gcloud --project=grey-sort-challenge functions deploy goWithTheDataFlow --stage-bucket gs://batch-pipeline --trigger-bucket gs://batch-pipeline
 //gcloud functions deploy goWithTheDataFlow --project=mrsutikno --region=australia-southeast1 --runtime=nodejs10 --set-env-vars DISABLE_LEGACY_METADATA_SERVER_ENDPOINTS=true --service-account=datadeployer@sutikno.iam.gserviceaccount.com --stage-bucket=gs://batch-pipeline-code --trigger-bucket=gs://batch-pipeline-code
-const { google } = require('googleapis');
-//var {google} = require('googleapis');
-const PROJECT_ID = "mrsutikno";
+const {google} = require('googleapis');
+exports.helloGCS = function(event, callback) {
+    const file = event.data;
 
-const createDataflowJob = (dataflow, projectId, file) => {
-  dataflow.projects.templates.create({
-      projectId: projectId,
-      resource: {
-          parameters: {
-              inputFile: `gs://${file.bucket}/${file.name}`
-          },
-          jobName: 'called-from-a-cloud-function-batch-pipeline-' + new Date().getTime(),
-          gcsPath: 'gs://batch-pipeline/template/pipeline'
 
-      }
-  }, (err, response) => {
-      if (err) console.error("Error running dataflow template: ", err);
-      console.log("Dataflow template response: ", response);
-  });
-}
+        google.auth.getApplicationDefault(function(err, authClient, projectId) {
+            if (err) {
+                throw err;
+            }
+            if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+                authClient = authClient.createScoped([
+                    'https://www.googleapis.com/auth/cloud-platform',
+                    'https://www.googleapis.com/auth/userinfo.email'
+                ]);
+            }
+            const dataflow = google.dataflow({
+                version: 'v1b3',
+                auth: authClient
+            });
+            dataflow.projects.templates.create({
+                projectId: projectId,
+                resource: {
+                    parameters: {
+                        inputFile: `gs://cloud-dataflow-bucket-input/*.txt`
+                    },
+                    jobName: 'dataflow-triggered-by-cloud-function',
+                    gcsPath: 'gs://cloud-dataflow-pipeline-bucket-staging/templates/dataflow-custom-redis-template'
+                }
+            }, function(err, response) {
+                if (err) {
+                    console.error("Problems occurred while running dataflow template, error was: ", err);
+                }
+                console.log("The dataflow template response: ", response);
+                callback();
+            });
+        });
 
-exports.goWithTheDataFlow = async function(data, context, callback) {
-  const file = data;  
-  //const etype = data.context.eventType;
-  const ttype = context.eventType;
-     
-  console.log("File is: ", file);
-  console.log("ttype is: ", ttype);
-
-  //if (etype === 'google.storage.object.finalize' && file.data.name.indexOf('upload/') !== -1) {
- if (ttype === 'google.storage.object.finalize' && file.name.indexOf('upload/') !== -1) {
-   
-    const authClient = await google.auth.getClient({
-      scopes: [
-        'https://www.googleapis.com/auth/cloud-platform',
-        'https://www.googleapis.com/auth/compute',
-        'https://www.googleapis.com/auth/userinfo.email' 
-        ]
-    }); 
-        
-
-    console.log("then here?");
-    const dataflow = google.dataflow({ version: 'v1b3', auth: authClient });
-    createDataflowJob(dataflow, PROJECT_ID, file);
-    callback();
-
-  } else {
-    console.log("Nothing to do here, ignoring.");
-    callback();
-  }
 };
