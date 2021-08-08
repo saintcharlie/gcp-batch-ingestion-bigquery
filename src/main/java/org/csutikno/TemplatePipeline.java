5a36fc929bf3ca9f3aa8a701d7855794ffe3b934
Charles Sutikno <charlessutikno@gmail.com>
2021-08-09 09:53:09 +1000

    test

--- src/main/java/org/csutikno/TemplatePipeline.java
+++ src/main/java/org/csutikno/TemplatePipeline.java
@@ -7,11 +7,16 @@ import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.CreateDisposition;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.ValueProvider;
+import org.apache.beam.sdk.options.Validation;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,13 +28,16 @@ import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposit
  * Do some randomness
  */
 public class TemplatePipeline {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TemplatePipeline.class);
+
     public static void main(String[] args) {
         PipelineOptionsFactory.register(TemplateOptions.class);
         TemplateOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(TemplateOptions.class);
         //DataflowPipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(TemplateOptions.class);
         Pipeline pipeline = Pipeline.create(options);
-        //pipeline.apply("READ", TextIO.read().from(options.getInputFile()))
-        pipeline.apply("READ", TextIO.read().from(options.getInputFilePattern()))
+        pipeline.apply("READ", TextIO.read().from(options.getInputFile()))
+        //pipeline.apply("READ", TextIO.read().from(options.getInputFilePattern()))
                 .apply("TRANSFORM", ParDo.of(new WikiParDo()))
                 .apply("WRITE", BigQueryIO.writeTableRows()
                         .to(String.format("%s:banking.transaction", options.getProject()))
